b60944e5a09d066def199bfb1d41fd98e005d720
sworc <lightbox@nifty.com>
2022-05-01 15:29:00 +0900

    Create Program.cs

---
+++ list/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace cs_0501_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
